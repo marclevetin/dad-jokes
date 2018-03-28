a6da2c3dcacd01469cfc45bae3f3cf6d78bf7926
marclevetin <marc@derbyhat.org>
2018-03-28 00:33:24 -0400

    removed material-ui beta.  Discovered a bug that's been around for 6 months.  Fixing it is out of scope for my project.  Moving to plan B

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, {Component} from 'react';
 
-// material-ui-beta stuff
-import CssBaseline from 'material-ui/CssBaseline';
-import Grid from 'material-ui/Grid';
-import Button from 'material-ui/Button';
-
 class App extends Component {
   constructor(props) {
     super(props)
@@ -72,32 +67,18 @@ class App extends Component {
     const allJokes = this.state.combo.map(joke => <p key={joke.id}>{joke.joke}</p>);
 
     return (<div className="App">
-      <CssBaseline/>
       <header className="App-header">
         <h1 className="App-title">Dad Joke Engine</h1>
       </header>
-      <Grid container spacing={16}>
-        <Grid item xs={12}>
           <h1>Jokes!</h1>
-        </Grid>
-        <Grid item xs={12} md={6}>
           <h2>Random hilarity</h2>
-          <Button variant="raised" color="primary">
-            Get a Random Joke
-          </Button>
           <button onClick={() => this.combinedJoke(this.state.searchTerm)}>Get a Random Joke</button>
-        </Grid>
-        <Grid item xs={12} md={6}>
           <h2>Search for a joke</h2>
           <form onSubmit={this.handleSubmit}>
             <input type='text' onChange={this.handleChange} value={this.state.searchTerm}/>
             <button type='submit'>Search</button>
           </form>
-        </Grid>
-        <Grid item xs={12}>
           {allJokes}
-        </Grid>
-      </Grid>
     </div>);
   }
 }
