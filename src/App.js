7531e7e54caaf97442986851bda4fbee293f6a11
marclevetin <marc@derbyhat.org>
2018-03-27 23:54:38 -0400

    installed material-ui beta.  Testing to see if it's ready for use.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+
+// material-ui-beta stuff
+import CssBaseline from 'material-ui/CssBaseline';
+import Grid from 'material-ui/Grid';
+import Button from 'material-ui/Button';
 
 class App extends Component {
   constructor(props) {
@@ -8,7 +11,12 @@ class App extends Component {
 
     this.state = {
       searchTerm: '',
-      combo: [{id: 1, joke: ''}]
+      combo: [
+        {
+          id: 1,
+          joke: 'Fetching a dad joke'
+        }
+      ]
     }
 
     this.combinedJoke = this.combinedJoke.bind(this);
@@ -21,28 +29,34 @@ class App extends Component {
   }
 
   combinedJoke = (term) => {
-    const url = (term) ? `https://icanhazdadjoke.com/search?term=${term}` : 'https://icanhazdadjoke.com/';
-    fetch(url, { headers: { 'Accept': 'application/json' } })
-      .then(response => {
-                          if (response.ok) {
-                            return response;
-                          } else {
-                            let errorMessage = `${response.status} (${response.statusText})`,
-                              error = new Error(errorMessage);
-                            throw (error);
-                          }
-                        })
-      .then(response => response.text())
-      .then(body => {
-                      const jokes = JSON.parse(body);
-                      const allJokes = (jokes.results) ? jokes.results : [{id: 1, joke: jokes.joke}];
+    const url = (term)
+      ? `https://icanhazdadjoke.com/search?term=${term}`
+      : 'https://icanhazdadjoke.com/';
+    fetch(url, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+          error = new Error(errorMessage);
+        throw(error);
+      }
+    }).then(response => response.text()).then(body => {
+      const jokes = JSON.parse(body);
+      const allJokes = (jokes.results)
+        ? jokes.results
+        : [
+          {
+            id: 1,
+            joke: jokes.joke
+          }
+        ];
 
-                      this.setState({
-                        combo: allJokes,
-                        searchTerm: ''
-                      });
-                    })
-      .catch(error => console.error(`Error in fetch: ${error.message}`));
+      this.setState({combo: allJokes, searchTerm: ''});
+    }).catch(error => console.error(`Error in fetch: ${error.message}`));
   }
 
   handleChange = (event) => {
@@ -55,27 +69,36 @@ class App extends Component {
   }
 
   render() {
-    const allJokes = this.state.combo.map(joke => <p key={joke.id} >{joke.joke}</p>);
+    const allJokes = this.state.combo.map(joke => <p key={joke.id}>{joke.joke}</p>);
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Dad Joke Engine</h1>
-        </header>
-
-        <h1>Jokes!</h1>
-        <h2>Random hilarity</h2>
-        <button onClick={() => this.combinedJoke(this.state.searchTerm)}>Get a Random Joke</button>
-        <h2>Search for a joke</h2>
-        <form onSubmit={this.handleSubmit}>
-          <input type='text' onChange={this.handleChange} value={this.state.searchTerm}/>
-          <button type='submit'>Search</button>
-        </form>
-
-        {allJokes}
-      </div>
-    );
+    return (<div className="App">
+      <CssBaseline/>
+      <header className="App-header">
+        <h1 className="App-title">Dad Joke Engine</h1>
+      </header>
+      <Grid container spacing={16}>
+        <Grid item xs={12}>
+          <h1>Jokes!</h1>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <h2>Random hilarity</h2>
+          <Button variant="raised" color="primary">
+            Get a Random Joke
+          </Button>
+          <button onClick={() => this.combinedJoke(this.state.searchTerm)}>Get a Random Joke</button>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <h2>Search for a joke</h2>
+          <form onSubmit={this.handleSubmit}>
+            <input type='text' onChange={this.handleChange} value={this.state.searchTerm}/>
+            <button type='submit'>Search</button>
+          </form>
+        </Grid>
+        <Grid item xs={12}>
+          {allJokes}
+        </Grid>
+      </Grid>
+    </div>);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const AppWithStyle = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
